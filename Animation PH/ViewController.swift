8ab01ad219ad7115ca5107b0a4f2929a902b365b
Evgeniy Ryshkov <eryshkov@gmail.com>
2018-12-17 11:07:15 +0300

    layout finished

--- Animation PH/ViewController.swift
+++ Animation PH/ViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tap: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func tapped(_ sender: UIButton) {
+        
+    }
+    
 }
 
