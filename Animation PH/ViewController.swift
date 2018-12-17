dd82d628deb3e16f1a6a2dd64dffbac537416311
Evgeniy Ryshkov <eryshkov@gmail.com>
2018-12-17 11:49:16 +0300

    prepare for animations

--- Animation PH/ViewController.swift
+++ Animation PH/ViewController.swift
@@ -9,15 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var tap: UIButton!
+    
+    var imageView: UIImageView!
+    var currentAnimation = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        
+        view.addSubview(imageView)
     }
 
     @IBAction func tapped(_ sender: UIButton) {
         
+        tap.isHidden = true
+        
+        UIView.animate(withDuration: 1, delay: 0, options: [], animations: {[unowned self] in
+            switch self.currentAnimation {
+            case 0:
+                break
+            default:
+                break
+            }
+        }) {[unowned self] (finished: Bool) in
+            self.tap.isHidden = false
+        }
+        
+        currentAnimation += 1
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
     }
     
 }
